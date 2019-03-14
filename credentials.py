b1a98a08c8b38155431c15ada670ffeec8e54e66
Kishan Mehta <kishan@mobifly.co.uk>
2019-03-14 09:14:07 +0530

    Adds disclaimer.

--- credentials.py
+++ credentials.py
@@ -1,3 +1,4 @@
+# This should be secret and not committed to Git but putting here just for demo purpose.
 API_URL = "https://affiliate-api.flipkart.net/affiliate/1.0/search.json?query={}"
 FP_AFFILIATE_TRACKING_ID = "arun92phoenix"
 FP_Token = "0b5647b2f5da4182b3d2314f384c16fe"
